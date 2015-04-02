d0788a423981bb216ac24deaf3cc2f12cf9c69c1
James Murphy <jmurphy4@mail.umw.edu>
2015-04-02 17:44:51 -0400

    holy shit

---
+++ commit.py
@@ -0,0 +1,22 @@
+# script to commit to a git repo for a given language
+
+import sys, os
+
+if sys.version_info >= (3,0):
+	raw_input = input
+
+language = raw_input("Language for this git repo? ").lower()
+
+if language == "python":
+	os.system("git add *.py && git commit -a")
+elif language == "c++":
+	os.system("git add *.cpp *.h && git commit -a")
+elif language == "lua":
+	os.system("git add *.lua && git commit -a")
+elif language == "perl":
+	os.system("git add *.pl && git commit -a")
+else:
+	sys.stdout.write("Language not specified in commit.py.\n")
+	exit()
+
+sys.stdout.write("Commit completed.\n")
