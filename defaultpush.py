35f3fff2c445a58d20110120d2339cf4d1c16015
James Murphy <jmurphy4@mail.umw.edu>
2015-04-02 22:56:13 -0400

    Cleaned console output.

--- defaultpush.py
+++ defaultpush.py
@@ -3,8 +3,8 @@ import os, sys
 if sys.version_info >= (3,0):
 	raw_input = input
 
-destination = raw_input("Target 'upstream' or 'origin'?").lower()
-branch = raw_input("Enter branch (leave blank for 'master'")
+destination = raw_input("Target 'upstream' or 'origin'? ").lower()
+branch = raw_input("Enter branch (leave blank for 'master') ")
 
 if branch == "":
 	branch = "master"
