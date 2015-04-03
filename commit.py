2134bf259e9bc5ac387dd98f7772b76066d36244
James Murphy <jmurphy4@mail.umw.edu>
2015-04-03 03:31:13 -0400

    Refactored conditionals and added a new feature.

--- commit.py
+++ commit.py
@@ -7,17 +7,29 @@ if sys.version_info >= (3,0):
 
 language = raw_input("Language for this git repo? ").lower()
 
-if language == "python" or language == "py":
+# this is admittedly a despicable number of conditionals,
+# but blame python for not having a fucking switch statement
+
+if language == "python" or "py":
 	os.system("git add *.py && git commit -a")
+
 elif language == "c++":
 	os.system("git add *.cpp *.h && git commit -a")
+
 elif language == "lua":
 	os.system("git add *.lua && git commit -a")
-elif language == "perl":
+
+elif language == "perl" or "pl":
 	os.system("git add *.pl && git commit -a")
-elif language == "hack" or language == "assembly" or language == "asm":
+
+elif language == "hack" or "assembly" or "asm":
 	os.system("git add *.hack *.asm && git commit -a")
+
 elif language == "hdl":
 	os.system("git add *.hdl && git commit -a")
+
 else:
 	sys.stdout.write("Language not specified in commit.py.\n")
+	i = raw_input("Would you like to add this language now? (Y/N): ")
+	if i.lower() == "y":
+		os.system("goshell && vim commit.py")
