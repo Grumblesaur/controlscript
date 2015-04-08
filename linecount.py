b4d5af992b33d253c9400493f2beaa90619f8c8c
James Murphy <jmurphy4@mail.umw.edu>
2015-04-07 22:25:00 -0400

    Conditionals drastically refactored.

--- linecount.py
+++ linecount.py
@@ -1,21 +1,32 @@
 import sys, os
 
+# compatibility between Python 2 and Python 3
 if sys.version_info >= (3,0):
 	raw_input = input
 
-if len(sys.argv) == 1:
+# check for file argument
+if len(sys.argv) == 1 or len(sys.argv) > 2:
+	# print usage instructions and bail out if given invalid arguments
 	sys.stdout.write("Usage:\n")
-	sys.stdout.write("'python linecount.py <input file>'\n")
+	sys.stdout.write("'linecount <input file>'\n")
 	sys.exit()
-else:
-	filename = sys.argv[1]
-	language = filename.split(".")[1]
 
+filename = sys.argv[1]
+
+if "." not in filename:
+	sys.stdout.write("No file extension!\n")
+	sys.exit()
+
+language = filename.split(".")[1]
+
+# open source file
 readfile = open(filename, 'r')
 
 comment = ""
-multistart = '`````'
+multistart = '`````' # dummy markers for languages w/o multiline comments
 multiend = '`````'
+
+# language style options
 
 if language == "c++" or "cpp" or "c" or "cxx" or "h" or "java":
 	comment = "//"
@@ -41,9 +52,11 @@ elif language == "r":
 	comment = "#"
 
 else:
+	# provide option for user to add unknown comment styles
 	sys.stdout.write("Language comment style unknown.\n")
 	ans = raw_input("Would you like to add it now? (Y/N) ").lower()
 	
+	# go there, open the file in vim	
 	if ans == 'y':
 		os.system("cd /home/james/Programming/python/linecounter")
 		os.system("vim linecount.py")
@@ -53,27 +66,35 @@ else:
 total = 0
 code = 0
 noncode = 0
-multiline = False
+isMulti = False
 
 for line in readfile:
-	# count indented comments as noncode
-	if (line.lstrip()[0:len(comment)-1] == comment) or (multiline == True):
-		noncode += 1
-	# count multiline comment start as noncode
-	elif multistart in line:
-		noncode += 1
-		multiline = True
-	# count multiline comment end as noncode
-	elif (multiline == True) and (multiend in line):
-		noncode += 1
-		multiline = False
-	# count empty lines as noncode
-	elif line.strip() == "":
-		noncode += 1
-	# count logical lines as code
+	# begin logic for nonempty lines
+	if line != "":
+		# begin logic for non-whitespace lines
+		if line.lstrip() != "":
+			# treat comment lines as noncode
+			temp = line.lstrip()
+			if temp[0:len(comment)-1] == comment:
+				noncode += 1
+			elif temp[0:len(multistart)-1] == multistart:
+				noncode += 1
+				isMulti = True
+			elif (multiend in line) and (isMulti == True):
+				noncode += 1
+				isMulti = False
+			elif (multiend not in line) and (isMulti == True):
+				noncode += 1
+			# treat all other lines as code
+			else:
+				code += 1
+		# treat whitespace lines as noncode
+		else:
+			noncode += 1
+	# treat empty lines as noncode
 	else:
-		code += 1
-
+		noncode += 1
+		
 # find total length of file
 total = code + noncode
 
