1699153751dd8a5c1d81f6564f8456340c2a980b
James Murphy <jmurphy4@mail.umw.edu>
2015-04-09 01:05:56 -0400

    Commented user.py

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+# Replace the contents of filepath and editor with the correct
+# directory path and text editor name
+
 class userinfo(object):
 	filepath = "/home/james/Programming/python/shellscripts"
 	editor = "vim"
