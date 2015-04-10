c8938e923bb459f961f0ed67b98093e904d5696f
James Murphy <jmurphy4@mail.umw.edu>
2015-04-10 15:47:09 -0400

    Improved convenience of commit edit option

--- commit.py
+++ commit.py
@@ -4,7 +4,13 @@ from user import userinfo
 if sys.version_info >= (3,0):
 	raw_input = input
 
-l = raw_input("Language for this git repo? ").lower()
+if len(sys.argv) != 2:
+	# when improper argument count is passed, print usage and exit
+	sys.stdout.write("Usage:\n")
+	sys.stdout.write("commit <language name or file extension w/o '.'\n")
+	sys.exit()
+
+l = sys.argv[1]
 
 # this is admittedly a despicable number of conditionals,
 # but blame python for not having a fucking switch statement
@@ -29,7 +35,25 @@ elif l == "hdl":
 
 else:
 	sys.stdout.write("Language not specified in commit.py.\n")
-	i = raw_input("Would you like to add this language now? (Y/N): ")
-	if i.lower() == "y":
+	ans = raw_input("Would you like to add this language now? (Y/N): ")
+	if ans.lower() == "y":
+		# directory where we are currently
+		prev = os.getcwd()
+		
+		# go to the script and edit it
 		os.system("cd %s" %userinfo.filepath)
 		os.system("%s commit.py" %userinfo.editor)
+		
+		# return to initial directory
+		os.system("cd %s" %prev)
+		
+		ans = raw_input("Attempt commit again? (Y/N): ")
+		if ans.lower == "y":
+			# attempt commit again
+			os.system("python %s/commit.py %s" %(userinfo.filepath, l))
+		else:
+			sys.stdout.write("Commit aborted.\n")
+			sys.exit()
+	else:
+		sys.stdout.write("Commit aborted.\n")
+		sys.exit()
