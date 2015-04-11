6aa00cac49894165a8baccb96ce9662c21dbf841
James Murphy <jmurphy4@mail.umw.edu>
2015-04-11 01:34:07 -0400

    Improved error handling. try-except block may or may not be unnecessary now.

--- linecount.py
+++ linecount.py
@@ -12,18 +12,22 @@ if len(sys.argv) != 2:
 	sys.stdout.write("'linecount.py <input file>'\n")
 	sys.exit()
 
-filename = sys.argv[1]
+f = sys.argv[1]
 
 # require known file extension to count lines of code
-if "." not in filename:
+if "." not in f:
 	sys.stdout.write("No file extension!\n")
 	sys.exit()
 
 # grab the file extension and use as language name
-language = filename.split(".")[1]
+language = f.split(".")[1]
+
+if not os.path.isfile(f):
+	sys.stdout.write("File does not exist!\n")
+	sys.exit()
 
 # open source file
-readfile = open(filename, 'r')
+readfile = open(f, 'r')
 
 comment = "" # guaranteed not to be empty during counting process
 multistart = '`````' # dummy markers for languages w/o multiline comments
@@ -72,14 +76,28 @@ elif language in lispLike:
 else:
 	# provide option for user to add unknown comment styles
 	sys.stdout.write("Language comment style unknown.\n")
-	ans = raw_input("Would you like to add it now? (Y/N) ").lower()
+	ans = raw_input("Would you like to add it now? (Y/N): ").lower()
 	
-	# go there, open the file in vim	
 	if ans == 'y':
+		# directory where we are currently
+		prev = os.getcwd()
+		
+		# go to the script and edit it
 		os.system("cd %s" %userinfo.filepath)
 		os.system("%s linecount.py" %userinfo.editor)
-		sys.exit()
-	
+		
+		# return to the initial directory
+		os.system("cd %s" %prev)
+		
+		ans2 = raw_input("Attempt linecount again? (Y/N): ")
+		if ans2.lower() == "y":
+			# attempt commit again
+			t = sys.argv[1]
+			os.system("python %s/linecount.py %s" %(userinfo.filepath, t))
+			sys.exit() # prevents infinitely recursive program calls
+		else:
+			sys.stdout.write("Linecount aborted.\n")
+			sys.exit()
 	else:
 		sys.stdout.write("Linecount aborted.\n")
 		sys.exit()
@@ -92,8 +110,14 @@ whitespaceLines = 0
 isMulti = False
 
 for line in readfile:
-	temp = line.split(comment)
-	
+	try:
+		temp = line.split(comment)
+	except:
+		e = sys.exc_info()[0]
+		sys.stdout.write("User did not add new language info to script.\n")
+		sys.stdout.write("Linecount aborted.\n")
+		sys.exit()
+
 	# empty line logic
 	if line == "":
 		whitespaceLines += 1
