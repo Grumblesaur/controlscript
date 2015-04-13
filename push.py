1dac8a744c8aad8c4b95161229fd0971fca7bf28
James Murphy <jmurphy4@mail.umw.edu>
2015-04-12 20:55:32 -0400

    Added error check condition to push.py

--- push.py
+++ push.py
@@ -6,6 +6,10 @@ if sys.version_info >= (3,0):
 destination = raw_input("Target '(u)pstream' or '(o)rigin'? ").lower()
 branch = raw_input("Enter branch (leave blank for 'master') ").lower()
 
+if destination == "":
+	sys.stdout.write("No destination selected!\n")
+	sys.exit()
+
 if branch == "" or "m":
 	branch = "master"
 
@@ -16,4 +20,4 @@ elif destination == "origin" or destination == "o":
 	os.system("git push origin %s" %branch)
 
 else:
-	sys.stdout.write("Destination not found.\n")
+	sys.stdout.write("Destination not found!\n")
