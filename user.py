b8859f0cf6845bbb6d1358aae1be63121021bd44
James Murphy <jmurphy4@mail.umw.edu>
2015-04-15 01:38:48 -0400

    Removed unnecessary code.

--- user.py
+++
@@ -1,6 +0,0 @@
-# Replace the contents of filepath and editor with the correct
-# directory path and text editor name
-
-class userinfo(object):
-	filepath = "/home/your_name/path/to/controlscript_repository"
-	editor = "text_editor_of_your_choice" # e.g. vim, vi, emacs, ...
